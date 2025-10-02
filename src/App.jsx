e6ab7557206dd740932e5d0f721f99ad36afd979
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-02 15:54:23 -0400

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>To-Do List:</h1>
+        <div className='list-container'>
+          <ul className='list'>
+              <li className='list-item'>Learn Japanese</li>
+              <li className='list-item'>Wash the dishes</li>
+              <li className='list-item'>Clean the Table</li>
+              <li className='list-item'>Walk the Dog</li>
+              <li className='list-item'>Reply to emails</li>
+          </ul>
+        </div>
+    </div>
+  )
+}
+
+export default App
